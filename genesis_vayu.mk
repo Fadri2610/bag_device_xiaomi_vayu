cc71418d19c48e3aedfa19f1074b6c1d907aab39
Fadri2610 <fabiflow102004@gmail.com>
2025-04-22 20:20:10 -0500

    Genify!

--- genesis_vayu.mk
+++ genesis_vayu.mk
@@ -11,20 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common DerpFest configurations
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit common Genesis configurations
+$(call inherit-product, vendor/genesis/config/common_full_phone.mk)
 
-PRODUCT_NAME := derp_vayu
+PRODUCT_NAME := genesis_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
 PRODUCT_MANUFACTURER := Xiaomi
-
-DERP_BUILDTYPE := Official
-DERP_VERSION_APPEND_TIME_OF_DAY=true
-TARGET_NOT_USES_BLUR=true
-USE_LEGACY_BOOTANIMATION := true
-TARGET_USES_MINI_GAPPS := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
